d32fa339a9de29837f97774c83b20909f1b2ee8a
Michael Pedersen <steele.pedersen@gmail.com>
2019-11-03 16:38:32 -0500

    update ports

--- run_all.sh
+++ run_all.sh
@@ -4,13 +4,13 @@ sudo pip install flask_apscheduler tensorflow transformers tqdm flask torch
 cd ~/gamejam-2019/FindRealHumansNearYouGame/gpt2api/src && sudo flask run --host 0.0.0.0 --port 8080 &
 
 # Run Flask API for Game State (port 9283)
-cd ~/gamejam-2019/FindRealHumansNearYouGame/flaskapi/src && sudo flask run --host 0.0.0.0 --port 465 &
+cd ~/gamejam-2019/FindRealHumansNearYouGame/flaskapi/src && sudo flask run --host 0.0.0.0 --port 80 &
 
 # Run CRUD API (port 9620)
 #cd ~/gamejam-2019/FindRealHumansNearYouGame/api/python-flask-server && python3 -m swagger_server &
 
 # ADMIN DASHBOARD (80)
-cd ~/gj/FindRealHumansNearYouGame/admindashboard/admin && sudo python2 -m SimpleHTTPServer 80 &
+# cd ~/gj/FindRealHumansNearYouGame/admindashboard/admin && sudo python2 -m SimpleHTTPServer 80 &
 
 #Status Output
 sleep 10
