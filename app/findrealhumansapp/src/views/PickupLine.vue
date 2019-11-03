ebde44c1ec552012bd0bce9e0d62ae4f71c686d8
Vuong Le <vuongle6@gmail.com>
2019-11-03 00:50:17 -0500

    navigation done for now

--- app/findrealhumansapp/src/views/PickupLine.vue
+++ app/findrealhumansapp/src/views/PickupLine.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is the pickup line page</h1>
+    <h1>Your Name</h1>
+    <div class="picture">
+      <img src="https://www.thiswaifudoesnotexist.net/example-196646.jpg">
+    </div>
+    <h1>Enter pickup line</h1>
+    <textarea rows="4" cols="50"></textarea>
+    <button v-on:click="submit" type="button">Submit</button>
   </div>
 </template>
+
+<script>
+  export default {
+    name:'pickupline',
+    components:{},
+    methods:{
+      submit: function (){
+        window.location.href='#/swipe'
+      }
+    }
+  }
+</script>
