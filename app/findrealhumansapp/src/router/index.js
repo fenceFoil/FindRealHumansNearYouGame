89f690c51e3d3919e5d942bda9641045f99c5b87
Vuong Le <vuongle6@gmail.com>
2019-11-02 22:38:21 -0500

    base pages and routes complete

--- app/findrealhumansapp/src/router/index.js
+++ app/findrealhumansapp/src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Profile from '../views/Profile.vue'
+import Results from '../views/Results.vue'
+import PickupLine from '../views/PickupLine.vue'
+import Swipe from '../views/Swipe.vue'
 
 Vue.use(VueRouter)
 
@@ -17,6 +21,26 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile
+  },
+  {
+    path: '/pickupline',
+    name: 'pickupline',
+    component: PickupLine
+  },
+  {
+    path: '/results',
+    name: 'results',
+    component: Results
+  },
+  {
+    path: '/swipe',
+    name: 'swipe',
+    component: Swipe
   }
 ]
 
