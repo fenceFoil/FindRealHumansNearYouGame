e85f5553be0c725a792665efb532c8ae5e50011b
fenceFoil <karnavasbj@gmail.com>
2019-11-06 22:44:52 -0500

    Installing more dependencies automatically

--- build_app_dist.sh
+++ build_app_dist.sh
@@ -1,2 +1,4 @@
+sudo apt install npm
+npm install -g @vue/cli
 cd ~/FindRealHumansNearYouGame/app/findrealhumansapp && npm run build && rm -rf ~/FindRealHumansNearYouGame/flaskapi/src/static/app/ && yes | cp -R ~/FindRealHumansNearYouGame/app/findrealhumansapp/dist/ ~/FindRealHumansNearYouGame/flaskapi/src/static/app/
 #cp -Rf ~/gamejam-2019/FindRealHumansNearYouGame/app/findrealhumansapp/dist ~/gamejam-2019/FindRealHumansNearYouGame/flaskapi/src/static/app/
