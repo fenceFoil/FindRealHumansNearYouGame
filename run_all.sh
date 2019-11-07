864be3ac7861d25c612b6fabf24af587d67dfbd7
fenceFoil <karnavasbj@gmail.com>
2019-11-06 22:09:38 -0500

    Simplifying deployment

--- run_all.sh
+++ run_all.sh
@@ -1,10 +1,10 @@
 sudo pip install flask_apscheduler tensorflow transformers tqdm flask torch
 
 # Run GPT2 API endpoint (port 5000)
-cd ~/gamejam-2019/FindRealHumansNearYouGame/gpt2api/src && sudo flask run --host 0.0.0.0 --port 8080 &
+cd ~/FindRealHumansNearYouGame/gpt2api/src && sudo flask run --host 0.0.0.0 --port 8080 &
 
 # Run Flask API for Game State (port 9283)
-cd ~/gamejam-2019/FindRealHumansNearYouGame/flaskapi/src && sudo flask run --host 0.0.0.0 --port 80 &
+cd ~/FindRealHumansNearYouGame/flaskapi/src && sudo flask run --host 0.0.0.0 --port 80 &
 
 # Run CRUD API (port 9620)
 #cd ~/gamejam-2019/FindRealHumansNearYouGame/api/python-flask-server && python3 -m swagger_server &
