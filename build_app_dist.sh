be6ac058000477f7c7991bc879e4bd9aee411ad0
Michael Pedersen <steele.pedersen@gmail.com>
2019-11-08 22:25:09 -0500

    build_app_dist doesn't assume Michael's computer!

--- build_app_dist.sh
+++ build_app_dist.sh
@@ -1,4 +1,3 @@
-sudo apt install npm
-sudo npm install -g @vue/cli
-cd ~/FindRealHumansNearYouGame/app/findrealhumansapp && npm run build && rm -rf ~/FindRealHumansNearYouGame/flaskapi/src/static/app/ && yes | cp -R ~/FindRealHumansNearYouGame/app/findrealhumansapp/dist/ ~/FindRealHumansNearYouGame/flaskapi/src/static/app/
-#cp -Rf ~/gamejam-2019/FindRealHumansNearYouGame/app/findrealhumansapp/dist ~/gamejam-2019/FindRealHumansNearYouGame/flaskapi/src/static/app/
+rootDir=`pwd`
+echo $rootDir
+cd $rootDir/app/findrealhumansapp && npm run build && rm -rf $rootDir/flaskapi/src/static/app/ && yes | cp -R $rootDir/app/findrealhumansapp/dist/ $rootDir/flaskapi/src/static/app/
