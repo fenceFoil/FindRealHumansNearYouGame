ab7b135142f9ba6df966bbdd60496f2b9efb68d8
vuongle6@gmail.com <vle7@uncc.edu>
2019-11-09 16:35:40 -0500

    actually put timer stuff in

---
+++ app/findrealhumansapp/src/components/Timer.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script>
+import { REST_BASE } from './../constants/constants.js'
+
+export default {
+  name: 'Timer',
+  props: {
+  },
+  data: function (){
+    return {
+
+     }
+  },
+  created: async function() {
+
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
