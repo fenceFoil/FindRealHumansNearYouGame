fc3fbfcbaee9e14791b49a624b2ac7dc9db94316
vuongle6@gmail.com <vle7@uncc.edu>
2019-11-09 16:35:26 -0500

    started timer component and put status bar in hud

--- app/findrealhumansapp/src/components/Hud.vue
+++ app/findrealhumansapp/src/components/Hud.vue
@@ -4,6 +4,9 @@
         v-on:mouseover="mouseover"
         v-on:mouseleave="mouseleave">Show HUD</div>
         <div id="hud">
+            <div id="status-bar">
+                <h1 id="status-bar-text">{{statusBarText}}</h1>
+            </div>
             <h1><pre>Game State:<br>{{gamestate}}</pre></h1>
             <!-- add in game info here -->
         </div>
@@ -11,14 +14,47 @@
 </template>
 
 <script>
-import { HUD_CONTROL } from './../constants/constants.js';
+import { HUD_CONTROL, OVERLAY_CONTROL, REST_BASE } from './../constants/constants.js';
 export default {
     name: 'Hud',
     props: {
     },
     data: function (){
         return {
+            statusBarText: ""
         }
+    },
+    created: async function() {
+    let that = this;
+    let currGameID = 0;
+
+    // Continuously poll the server for the current game state
+    setInterval(async function() {
+            const response3 = await fetch(REST_BASE + "/game_state", {
+                method: "GET"
+            });
+            if (response3.status != 200) {
+                this.statusBarText =
+                "Cannot reach game" + (REST_BASE != "")
+                    ? " at " + REST_BASE
+                    : " on same server as this page";
+            } else {
+                let serverState = await response3.json();
+
+                // Respond to new server state
+
+                // Update status bar message
+                that.statusBarText = serverState.message + " -- " + serverState.countdownSecs;
+
+                // Check for new game
+                if (serverState.gameID != currGameID) {
+                // Game was reset!
+                window.location.href='#/'
+                OVERLAY_CONTROL.OFF();
+                currGameID = serverState.gameID
+                }
+            }
+        }, 1000);
     },
     methods: {
         mouseover: function(){
@@ -51,5 +87,20 @@ export default {
         background-color: rgba(0,0,0,0); /* Black background with opacity */
         z-index: -1; /* Specify a stack order in case you're using a different order for other elements */
         cursor: pointer; /* Add a pointer on hover */
+    }
+    
+    #status-bar {
+        background-color: #a55fc1;
+        z-index: 3;
+        text-align: center;
+    }
+
+    #status-bar-text {
+        font-family: Crayon, Helvatica, Arial, sans-serif;
+        margin: 0;
+        padding: 0.1em;
+        margin-bottom: 0.3em;
+        font-size: 1.8em;
+        color: #1a232c;
     }
 </style>
