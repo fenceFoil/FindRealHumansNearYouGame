9b91797546debd29b5d5dfdeeacde39c61710d4e
Michael Pedersen <steele.pedersen@gmail.com>
2019-11-09 00:22:40 -0500

    update scripts

--- server_scripts/update.sh
+++ server_scripts/update.sh
@@ -1,2 +1,4 @@
 # Reset Git, Pull updates, build app dist, and start everything
-git clean -fd && git pull && ./build_app_dist.sh && ./run_all.sh
+git clean -fd && git pull && ./build_app_dist.sh
+
+echo "If you wish to restart the server re-run run_all.sh"
