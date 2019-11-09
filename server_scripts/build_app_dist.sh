6fad3cec44d09b0e41084fbebeb85671b47628c4
Michael Pedersen <steele.pedersen@gmail.com>
2019-11-09 00:16:13 -0500

    server scripts are now run and done

--- server_scripts/build_app_dist.sh
+++ server_scripts/build_app_dist.sh
@@ -1,3 +1,3 @@
 rootDir=`cd .. && pwd`
 echo $rootDir
-cd $rootDir/app/findrealhumansapp && npm run build && rm -rf $rootDir/flaskapi/src/static/app/ && yes | cp -R $rootDir/app/findrealhumansapp/dist/ $rootDir/flaskapi/src/static/app/
+cd $rootDir/app/findrealhumansapp && npm install && npm run build && rm -rf $rootDir/flaskapi/src/static/app/ && yes | cp -R $rootDir/app/findrealhumansapp/dist/ $rootDir/flaskapi/src/static/app/
--- server_scripts/server_setup.sh
+++ server_scripts/server_setup.sh
@@ -1,3 +1,21 @@
-sudo apt install npm nodejs
+sudo apt-get update
+sudo apt-get install gcc g++ make
+
+# Install Nodejs & npm
+curl -sL https://deb.nodesource.com/setup_13.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+# Install YARN
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update && sudo apt-get install yarn
+
 sudo ln -s /usr/bin/nodejs /usr/bin/node
 sudo npm install --no-optional -g @vue/cli
+
+# Install npm dependencies
+cd $rootDir/app/findrealhumansapp && npm install
+
+#  @vue/cli-service @vue/cli-plugin-babel @vue/cli-service-global
+# sudo npm install --no-optional -g vue eslint @vue/cli @vue/cli-service @vue/cli-plugin-babel @vue/cli-service-global @vue/cli-plugin-eslint vue-template-compiler eslint-plugin-vue^C
+# sudo apt-get remove -y npm node yarn
