b708ecab829daa855e2169215857fb6c54fcfc15
Michael Pedersen <steele.pedersen@gmail.com>
2019-11-09 00:53:21 -0500

    hard reset git on update

--- server_scripts/update.sh
+++ server_scripts/update.sh
@@ -1,4 +1,4 @@
 # Reset Git, Pull updates, build app dist, and start everything
-git clean -fd && git pull && ./build_app_dist.sh
+git clean -fd && git reset --hard origin/master && git pull && ./build_app_dist.sh
 
 echo "If you wish to restart the server re-run run_all.sh"
