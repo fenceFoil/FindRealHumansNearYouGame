098a214ee0c836fe41309f4a843242987c3fabd1
fenceFoil <karnavasbj@gmail.com>
2019-11-09 20:01:05 -0500

    Forgot yet another slash. Do you read slash?

--- flaskapi/src/app.py
+++ flaskapi/src/app.py
@@ -11,7 +11,7 @@ VERSION = 8
 SWIPING_SECONDS = 60
 WRITING_PICKUPS_SECONDS = 60
 NUM_ROUNDS = 4
-GPT2_URL = "http:/findrealhumansnearyou.com/"
+GPT2_URL = "http://findrealhumansnearyou.com/"
 
 app = Flask(__name__)
 CORS(app)
