e06adfbad00ad111e5a51e8a9bb5fa4026f7cb9c
vuongle6@gmail.com <vle7@uncc.edu>
2019-11-11 17:35:13 -0500

    more speech bubbles

--- app/findrealhumansapp/src/views/PickupLine.vue
+++ app/findrealhumansapp/src/views/PickupLine.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="about">
     <h1 class="header-title">{{playerName}}</h1>
+    <template v-if="settingPickupLine">
+      <h1>Enter pickup line</h1>
+    </template>
+    <template v-if="selectingOption">
+      <h1>Enhance Your Pickup Line</h1>
+    </template>
     <h1 class="this-is-you japanese">これはあなたです</h1>
     <div class="left-right">👉</div>
     <div class="left-right-reverse">👉</div>
@@ -9,16 +15,15 @@
       <img class="waifu" :src="pictureURL">
     </div>
     <template v-if="settingPickupLine">
-      <h1>Enter pickup line</h1>
-      <div class="text">
-        <textarea v-model="pickupline" rows="4" cols="50"></textarea>
+      <div class="text speech-bubble">
+        <div class="arrow top right"></div>
+        <textarea id="pickupLine-input" v-model="pickupline" rows="4" cols="40"></textarea>
       </div>
       <div class="button">
         <button v-on:click="submit" type="button">Submit</button>
       </div>
     </template>
     <template v-if="selectingOption">
-      <h1>Enhance Your Pickup Line</h1>
       <div v-for="ending in endings" :key="ending" class="text">
         <div class="button">
           <button v-on:click="enhanceLine(ending)" type="button">{{ending}}</button>
@@ -88,3 +93,50 @@
     }
   }
 </script>
+
+<style scoped>
+  .speech-bubble{
+    background-color: #f8f8f8;
+    border: 2px solid #c8c8c8;
+    border-radius: .5em;
+    width: 50%;
+    text-align: center;
+    padding: 20px;
+    /* position: absolute; */
+    margin-left: 25%;
+    margin-top: 1em;
+    font-size: 3em;
+
+  }
+
+  .arrow {
+    border-style: solid;
+    position: absolute;
+    border-color: transparent transparent #c8c8c8 transparent;
+    border-width: 0em .8em .8em .8em;
+    margin-top: -1.25em;
+    margin-left: -.4em;
+    left: 50%;
+  }
+
+  .arrow:after {
+    border-color: transparent transparent #f8f8f8 transparent;
+    border-style: solid;
+    border-width: 0px 1em 1em 1em;
+    top: 3px;
+    content: "";
+    position: absolute;
+    left: -1em
+  }
+
+  #pickupLine-input{
+    resize: vertical;
+    font-size: 1em;
+    border: none;
+    background-color: #f8f8f8;
+    outline: none;
+    font-family: Glitter;
+    overflow: hidden;
+    text-align: center
+  }
+</style>
