8aa48f6dc454f4fc0d6c53ce8dbc408d45c6cc5a
vuongle6@gmail.com <vle7@uncc.edu>
2019-12-19 16:00:21 -0500

    prevent interactions with hidden stuff

--- app/findrealhumansapp/src/App.vue
+++ app/findrealhumansapp/src/App.vue
@@ -2,8 +2,8 @@
   <div id="overall">
     <LoadingOverlay></LoadingOverlay>
     <Hud></Hud>
+    <div id="background"></div>
     <div id="vertical-app-layout">
-
       <div id="app">
         <router-view />
       </div>
@@ -101,6 +101,20 @@ body {
   margin-bottom: 0.3em;
   font-size: 1.8em;
   color: #1a232c;
+}
+
+#background {
+    position: fixed; /* Sit on top of the page content */
+    width: 100%; /* Full width (cover the whole page) */
+    height: 100%; /* Full height (cover the whole page) */
+    display: block;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0,0,0,0); /* Black background with opacity */
+    z-index: -2;  /* Specify a stack order in case you're using a different order for other elements */
+    cursor: pointer; /* Add a pointer on hover */
 }
 
 button, input, textarea{
